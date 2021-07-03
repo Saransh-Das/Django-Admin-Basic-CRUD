db6977df655444fb6848af8c4c96b4d599d704b7
Saransh <dassaransh@gmail.com>
2021-07-03 22:22:37 +0530

    Version 3.0 Added Styling and Buttons

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Employee(models.Model):
     eid = models.CharField(max_length=20)
+    eclass = models.CharField(max_length=100 , null=True , blank=False)
     ename = models.CharField(max_length=100)
     eemail = models.EmailField()
     econtact = models.CharField(max_length=30)
